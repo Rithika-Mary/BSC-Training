eb889620f6e3e557b60678ccc2d610e356f94b6d
Rithika-Mary <rithika.mary@bsc-us.co.in>
2026-06-25 23:17:27 +0530

    Added Day 1 comprehension exercises

---
+++ Day 1/comprehension.py
@@ -0,0 +1,9 @@
+# List Comprehension
+numbers = [1, 2, 3, 4, 5]
+squares = [n*n for n in numbers]
+print("Squares:", squares)
+
+# Dictionary Comprehension
+square_dict = {n: n*n for n in numbers}
+print("Dictionary:", square_dict)
+
